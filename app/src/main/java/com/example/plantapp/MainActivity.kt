1d53bfffa0267406ccf04535b1e125cfd9921ef3
megan.archibald@umontana.edu <megan.archibald@umontana.edu>
2022-11-28 20:28:49 -0700

    fixes

--- app/src/main/java/com/example/plantapp/MainActivity.kt
+++ app/src/main/java/com/example/plantapp/MainActivity.kt
@@ -25,8 +25,6 @@ import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
-    // Binding object instance corresponding to the activity_main.xml layout
-    // when the view hierarchy is attached to the fragment.
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
@@ -35,8 +33,6 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // TODO: Retrieve NavController from the NavHostFragment
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
--- app/src/main/java/com/example/plantapp/model/OrderViewModel.kt
+++ app/src/main/java/com/example/plantapp/model/OrderViewModel.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2021 The Android Open Source Project.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.plantapp.model
 
 import androidx.lifecycle.LiveData
@@ -49,55 +35,27 @@ class OrderViewModel : ViewModel() {
     val total: LiveData<String> = Transformations.map(_total) {
         NumberFormat.getCurrencyInstance().format(it)
     }
-
-
-    /**
-     * Set the entree for the order.
-     */
     fun setPlant(plant: String) {
-        // TODO: if _entree.value is not null, set the previous entree price to the current
-        //  entree price.
+        // if _plant.value is not null,the previous plant price will be set to the current plant price.
         previousPlantPrice = _plant.value?.price ?: 0.0
 
-        // TODO: if _subtotal.value is not null subtract the previous entree price from the current
-        //  subtotal value. This ensures that we only charge for the currently selected entree.
         _subtotal.value = (_subtotal.value ?: 0.0) - previousPlantPrice
 
-        // TODO: set the current entree value to the menu item corresponding to the passed in string
+        // this sets the current plant value to the plant item corresponding to the passed in string
         _plant.value = plantItems[plant]
-        // TODO: update the subtotal to reflect the price of the selected entree.
+        // this updates the subtotal to reflect the price of the selected plant.
         updateSubtotal(plantItems[plant]?.price ?:0.0)
     }
 
-    /**
-     * Set the side for the order.
-     */
     fun setDelivery(delivery: String) {
-        // TODO: if _side.value is not null, set the previous side price to the current side price.
         previousDeliveryPrice = _delivery.value?.price ?: 0.0
-
-        // TODO: if _subtotal.value is not null subtract the previous side price from the current
-        //  subtotal value. This ensures that we only charge for the currently selected side.
         _subtotal.value = (_subtotal.value ?: 0.0) - previousDeliveryPrice
 
-        // TODO: set the current side value to the menu item corresponding to the passed in string
         _delivery.value = plantItems[delivery]
-        // TODO: update the subtotal to reflect the price of the selected side.
         updateSubtotal(plantItems[delivery]?.price ?:0.0)
     }
 
-    /**
-     * Set the accompaniment for the order.
-     */
-
-
-    /**
-     * Update subtotal value.
-     */
     private fun updateSubtotal(itemPrice: Double) {
-        // TODO: if _subtotal.value is not null, update it to reflect the price of the recently
-        //  added item.
-        //  Otherwise, set _subtotal.value to equal the price of the item.
         if (_subtotal.value != null){
             _subtotal.value = (_subtotal.value)?.plus(itemPrice)
         }
@@ -105,20 +63,11 @@ class OrderViewModel : ViewModel() {
             _subtotal.value = itemPrice
         }
 
-        // TODO: calculate the tax and resulting total
+
 
     }
 
-    /**
-     * Calculate tax and update total.
-     */
-
-
-    /**
-     * Reset all values pertaining to the order.
-     */
     fun resetOrder() {
-        // TODO: Reset all values associated with an order
         previousPlantPrice = 0.0
         previousDeliveryPrice = 0.0
         _plant.value = null
